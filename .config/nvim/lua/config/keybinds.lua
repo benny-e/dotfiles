43b4fa3826ea4afcef772cdb2f203b0cd9e2e7b6
Benny <bennett.g.estes@gmail.com>
2026-03-06 17:33:25 -0600

    Other updates

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -3,3 +3,5 @@ vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 -- Set jump between splits to just crtl + l and ctrl + h
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+-- Fugitive keybind
+vim.keymap.set("n", "<leader>g", ":Git<CR>", { desc = "Open Fugitive Git" })
