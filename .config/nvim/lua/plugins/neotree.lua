b5c921262bb9287e80b843711cf6c0fdb06a6c48
Benny <bennett.g.estes@gmail.com>
2026-03-10 18:01:02 -0500

    move neotree to right side

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -1,37 +1,37 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    opts = {
-      filesystem = {
-        follow_current_file = { enabled = true },
-        hijack_netrw_behavior = "open_default",
-	filtered_items = {
-	 	    visible = true,
-		    hide_dotfiles = false,
-		    hide_gitignored = false,
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
 		},
-      },
-      window = {
-        position = "left",
-        width = 32,
-        mappings = {
-          ["<CR>"] = "open",
-          ["l"] = "open",
-          ["h"] = "close_node",
-        },
-      },
-    },
-    config = function(_, opts)
-      require("neo-tree").setup(opts)
+		opts = {
+			filesystem = {
+				follow_current_file = { enabled = true },
+				hijack_netrw_behavior = "open_default",
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				},
+			},
+			window = {
+				position = "right",
+				width = 32,
+				mappings = {
+					["<CR>"] = "open",
+					["l"] = "open",
+					["h"] = "close_node",
+				},
+			},
+		},
+		config = function(_, opts)
+			require("neo-tree").setup(opts)
 
-      vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>", { desc = "Neo-tree: toggle" })
-      vim.keymap.set("n", "<leader>E", "<cmd>Neotree focus<cr>", { desc = "Neo-tree: focus" })
-    end,
-  },
+			vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>", { desc = "Neo-tree: toggle" })
+			vim.keymap.set("n", "<leader>E", "<cmd>Neotree focus<cr>", { desc = "Neo-tree: focus" })
+		end,
+	},
 }
