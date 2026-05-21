0a513db8bb1ebb9b3153b02dffa948f199d3904c
Benny <bennett.g.estes@gmail.com>
2026-05-21 18:35:57 -0500

    Niri Updates

--- .config/niri/scripts/idle.sh
+++ .config/niri/scripts/idle.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 exec swayidle -w \
-  timeout 300 'pidof hyprlock || hyprlock' \
+  timeout 300 'swaylock' \
   timeout 600 'niri msg action power-off-monitors' \
   resume 'niri msg action power-on-monitors' \
-  before-sleep 'pidof hyprlock || hyprlock'
+  before-sleep 'swaylock'
